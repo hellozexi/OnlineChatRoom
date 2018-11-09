2e283768e95d8dfbe20314579586c9a8a575e69b
Mike Liu <mike@MikedeMacBook-Pro.local>
2018-11-09 15:28:26 -0600

    chat.ts changes

--- src/frontend/chat.ts
+++ src/frontend/chat.ts
@@ -1,16 +1,10 @@
 const socket = (window as any).io();
+socket.on("messageFromServer", messageReceived)
 
-class Chat {
-    static io : any;
-    constructor(private msg : string, private cb : Function){
-        Chat.io.on(msg, this.cb)
-    }
-    emmitMessage(message : string) {
-        Chat.io.emit('message', message)
-    }
-
-}
-Chat.io = socket;
+/**
+ * handle messages
+ * @param response
+ */
 function messageReceived(response : any) {
     console.log(response);
     let room = document.createElement("li");
@@ -18,14 +12,17 @@ function messageReceived(response : any) {
     $("#rooms").append(room);
 
 }
-let chat : Chat = new Chat('messageFromServer',messageReceived);
+/**
+ * emmit messages
+ */
+//create new rooms, when you click the button, rooms will show on the webpage
 function createNewRoom(){
     let message = (<HTMLInputElement>document.getElementById("newRoom")).value;
     if(message === "") {
         return;
     }
     document.getElementById("newRoom");
-    chat.emmitMessage(message);
+    socket.emit("message", message);
     $("#newRoom").val("");
 }
-document.getElementById("newRoom_btn").addEventListener("click", createNewRoom)
+document.getElementById("newRoom_btn").addEventListener("click", createNewRoom);
