2cf6af6c0b338232dc2dc34db24b74316ceb0535
jason <zjxuan1996@outlook.com>
2018-11-10 10:48:39 -0600

    serve static file

--- src/src/router/router.ts
+++ src/src/router/router.ts
@@ -1,10 +1,12 @@
 import {Router, Request, Response} from 'express';
+import * as express from 'express';
 import path = require("path");
 
 //import '../../www/client.css'
 const router = Router();
-router.get('/', function (req, res) {
-    res.sendFile("index.html");
-});
+router.use('/', express.static(path.resolve('/index.html')));
+// router.get('/', function (req, res) {
+//     res.sendFile("index.html");
+// });
 
 export default router;
--- src/test/model.test/chatroom.test.ts
+++ src/test/model.test/chatroom.test.ts
@@ -1,9 +1,25 @@
-import {ChatRoom} from "../../src/model";
+import {ChatRoom, User} from "../../src/model";
 
 
 test('test chatroom without admin', () => {
     let chatroom = new ChatRoom('public hall', null);
     expect(chatroom.users).toEqual({});
     expect(chatroom.admin).toBeNull();
+    expect(chatroom.name).toEqual('public hall');
+});
+
+test('test chatroom with admin', () => {
+    let user = new User('jason', 'mock socket id');
+    let chatroom = new ChatRoom('public hall', user);
+    expect(chatroom.users).toEqual({});
+    expect(chatroom.admin).toBe(user);
+    expect(chatroom.name).toEqual('public hall');
+});
+
+test('test join chat room', () => {
+    let user = new User('jason', 'mock socket id');
+    let chatroom = new ChatRoom('public hall', user);
+    expect(chatroom.users).toEqual({});
+    expect(chatroom.admin).toBe(user);
     expect(chatroom.name).toEqual('public hall');
 });
