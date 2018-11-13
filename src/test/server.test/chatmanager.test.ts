7e6bc343891cbad076f41e799580f7a56c41ebf1
jason <zjxuan1996@outlook.com>
2018-11-12 22:37:34 -0600

    reorganize test code

--- src/test/server.test/chatmanager.test.ts
+++ src/test/server.test/chatmanager.test.ts
@@ -1,7 +1,8 @@
 import {ChatManager} from "../../src/server/chatmanager";
 import {User} from "../../src/model";
 
-describe('Test the ChatManager', () => {
+
+describe('test log in', () => {
     let manager :ChatManager;
     let socketId = 'mock socket id';
     let user: User;
@@ -16,6 +17,40 @@ describe('Test the ChatManager', () => {
         let user_by_id = manager.getUserByID(socketId);
         expect(user_by_id.roomname).toBe('public hall');
     });
+});
+
+describe('test user logout', () => {
+    let manager :ChatManager;
+    let user: User;
+
+    beforeEach(() => {
+        manager = new ChatManager();
+        user = new User('jason', 'mock socket id');
+    });
+
+    test('test user logout successfully', () => {
+        expect(manager.hasUserId(user.socketId)).toBeFalsy();
+        manager.login(user);
+        expect(manager.hasUserId(user.socketId)).toBeTruthy();
+        expect(manager.hasUserId('fake')).toBeFalsy();
+
+        manager.logout(user);
+        expect(manager.hasUserId(user.socketId)).toBeFalsy();
+        expect(manager.hasUserName(user.name)).toBeFalsy();
+    });
+});
+
+describe('test displaying information', () => {
+    let manager :ChatManager;
+    let user: User, jessy: User, ben: User, fray: User;
+
+    beforeEach(() => {
+        manager = new ChatManager();
+        user = new User('jason', 'mock socket id');
+        jessy = new User('jessy', '1231224');
+        ben = new User('ben', '13432532');
+        fray = new User('fray', '2149730742');
+    });
 
     test('tell if user exist', () => {
         expect(manager.hasUserId(user.socketId)).toBeFalsy();
@@ -27,47 +62,7 @@ describe('Test the ChatManager', () => {
         expect(manager.hasUserName('fake name')).toBeFalsy();
     });
 
-    test('test user logout', () => {
-        expect(manager.hasUserId(user.socketId)).toBeFalsy();
-        manager.login(user);
-        expect(manager.hasUserId(user.socketId)).toBeTruthy();
-        expect(manager.hasUserId('fake')).toBeFalsy();
-
-        manager.logout(user);
-        expect(manager.hasUserId(user.socketId)).toBeFalsy();
-        expect(manager.hasUserName(user.name)).toBeFalsy();
-    });
-
-    test('test create room', () => {
-        manager.addRoom(user, 'room');
-        let room = manager.rooms['room'];
-        expect(room.name).toEqual('room');
-        expect(room.admin).toBe(user);
-    });
-
-    test('test switch room', () => {
-        manager.login(user);
-        manager.addRoom(user, 'room');
-
-        expect(user.roomname).toEqual('public hall');
-        expect(manager.rooms['public hall'].users).toContain(user);
-        expect(manager.rooms['room'].users).toHaveLength(0);
-
-        manager.switchRoom(user, 'room');
-
-        // user's room name changed
-        expect(user.roomname).toEqual('room');
-        // new room contains user
-        expect(manager.rooms['room'].users).toContain(user);
-        // old room doesn't contain user
-        expect(manager.rooms['public hall'].users).toHaveLength(0);
-    });
-
     test('test display users', () => {
-        let jessy = new User('jessy', '1231224');
-        let ben = new User('ben', '13432532');
-        let fray = new User('fray', '2149730742');
-
         expect(manager.usersInRoom('public hall')).toHaveLength(0);
         expect(manager.login(user)).toBeTruthy();
         expect(manager.usersInRoom('public hall')).toContain(user);
@@ -97,9 +92,54 @@ describe('Test the ChatManager', () => {
         expect(manager.usersInRoom('public hall')).toContain(ben);
         expect(manager.usersInRoom('public hall')).toHaveLength(3);
     });
+});
+
+describe('test chat room', () => {
+    let manager :ChatManager;
+    let user: User;
+
+    beforeEach(() => {
+        manager = new ChatManager();
+        user = new User('jason', 'mock socket id');
+    });
+
+    test('test create room', () => {
+        manager.addRoom(user, 'room');
+        let room = manager.rooms['room'];
+        expect(room.name).toEqual('room');
+        expect(room.admin).toBe(user);
+    });
+
+    test('test switch room', () => {
+        manager.login(user);
+        manager.addRoom(user, 'room');
+
+        expect(user.roomname).toEqual('public hall');
+        expect(manager.rooms['public hall'].users).toContain(user);
+        expect(manager.rooms['room'].users).toHaveLength(0);
+
+        manager.switchRoom(user, 'room');
+
+        // user's room name changed
+        expect(user.roomname).toEqual('room');
+        // new room contains user
+        expect(manager.rooms['room'].users).toContain(user);
+        // old room doesn't contain user
+        expect(manager.rooms['public hall'].users).toHaveLength(0);
+    });
+});
+
+describe('Test admin privilege', () => {
+    let manager :ChatManager;
+    let user: User, fray: User;
+
+    beforeEach(() => {
+        manager = new ChatManager();
+        user = new User('jason', 'mock socket id');
+        fray = new User('fray', '2149730742');
+    });
 
     test('test ban user', () => {
-        let fray = new User('fray', '2149730742');
         expect(manager.login(user)).toBeTruthy();
         expect(manager.login(fray)).toBeTruthy();
         expect(manager.addRoom(user, 'room')).toBeTruthy();
@@ -116,7 +156,6 @@ describe('Test the ChatManager', () => {
     });
 
     test('test kick user out', () => {
-        let fray = new User('fray', '2149730742');
         expect(manager.login(user)).toBeTruthy();
         expect(manager.login(fray)).toBeTruthy();
         expect(manager.addRoom(user, 'room')).toBeTruthy();
